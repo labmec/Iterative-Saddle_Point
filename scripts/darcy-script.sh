0b8dcbcbf485217b3f3550b8539e7474ec709d7e
giavancini <giovanea@unicamp.br>
2024-09-10 17:24:07 -0300

    Tracking scripts

---
+++ scripts/darcy-script.sh
@@ -0,0 +1,71 @@
+./iterative-condensed-darcy 5 2 1 1.0e+1
+./iterative-condensed-darcy 5 2 1 1.0e-0
+./iterative-condensed-darcy 5 2 1 1.0e-1
+./iterative-condensed-darcy 5 2 1 1.0e-2
+./iterative-condensed-darcy 5 2 1 1.0e-3
+./iterative-condensed-darcy 5 2 1 1.0e-4
+./iterative-condensed-darcy 5 2 1 1.0e-5
+./iterative-condensed-darcy 5 2 1 1.0e-6
+
+./iterative-condensed-darcy 10 2 1 1.0e+1
+./iterative-condensed-darcy 10 2 1 1.0e-0
+./iterative-condensed-darcy 10 2 1 1.0e-1
+./iterative-condensed-darcy 10 2 1 1.0e-2
+./iterative-condensed-darcy 10 2 1 1.0e-3
+./iterative-condensed-darcy 10 2 1 1.0e-4
+./iterative-condensed-darcy 10 2 1 1.0e-5
+./iterative-condensed-darcy 10 2 1 1.0e-6
+
+./iterative-condensed-darcy 15 2 1 1.0e+1
+./iterative-condensed-darcy 15 2 1 1.0e-0
+./iterative-condensed-darcy 15 2 1 1.0e-1
+./iterative-condensed-darcy 15 2 1 1.0e-2
+./iterative-condensed-darcy 15 2 1 1.0e-3
+./iterative-condensed-darcy 15 2 1 1.0e-4
+./iterative-condensed-darcy 15 2 1 1.0e-5
+./iterative-condensed-darcy 15 2 1 1.0e-6
+
+./iterative-condensed-darcy 20 2 1 1.0e+1
+./iterative-condensed-darcy 20 2 1 1.0e-0
+./iterative-condensed-darcy 20 2 1 1.0e-1
+./iterative-condensed-darcy 20 2 1 1.0e-2
+./iterative-condensed-darcy 20 2 1 1.0e-3
+./iterative-condensed-darcy 20 2 1 1.0e-4
+./iterative-condensed-darcy 20 2 1 1.0e-5
+./iterative-condensed-darcy 20 2 1 1.0e-6
+
+./iterative-condensed-darcy 25 2 1 1.0e+1
+./iterative-condensed-darcy 25 2 1 1.0e-0
+./iterative-condensed-darcy 25 2 1 1.0e-1
+./iterative-condensed-darcy 25 2 1 1.0e-2
+./iterative-condensed-darcy 25 2 1 1.0e-3
+./iterative-condensed-darcy 25 2 1 1.0e-4
+./iterative-condensed-darcy 25 2 1 1.0e-5
+./iterative-condensed-darcy 25 2 1 1.0e-6
+
+./iterative-condensed-darcy 30 2 1 1.0e+1
+./iterative-condensed-darcy 30 2 1 1.0e-0
+./iterative-condensed-darcy 30 2 1 1.0e-1
+./iterative-condensed-darcy 30 2 1 1.0e-2
+./iterative-condensed-darcy 30 2 1 1.0e-3
+./iterative-condensed-darcy 30 2 1 1.0e-4
+./iterative-condensed-darcy 30 2 1 1.0e-5
+./iterative-condensed-darcy 30 2 1 1.0e-6
+
+./iterative-condensed-darcy 35 2 1 1.0e+1
+./iterative-condensed-darcy 35 2 1 1.0e-0
+./iterative-condensed-darcy 35 2 1 1.0e-1
+./iterative-condensed-darcy 35 2 1 1.0e-2
+./iterative-condensed-darcy 35 2 1 1.0e-3
+./iterative-condensed-darcy 35 2 1 1.0e-4
+./iterative-condensed-darcy 35 2 1 1.0e-5
+./iterative-condensed-darcy 35 2 1 1.0e-6
+
+./iterative-condensed-darcy 40 2 1 1.0e+1
+./iterative-condensed-darcy 40 2 1 1.0e-0
+./iterative-condensed-darcy 40 2 1 1.0e-1
+./iterative-condensed-darcy 40 2 1 1.0e-2
+./iterative-condensed-darcy 40 2 1 1.0e-3
+./iterative-condensed-darcy 40 2 1 1.0e-4
+./iterative-condensed-darcy 40 2 1 1.0e-5
+./iterative-condensed-darcy 40 2 1 1.0e-6
---
+++ scripts/stokes-script.sh
@@ -0,0 +1,71 @@
+./iterative-condensed-stokes 5 2 1 1.0e+1
+./iterative-condensed-stokes 5 2 1 1.0e-0
+./iterative-condensed-stokes 5 2 1 1.0e-1
+./iterative-condensed-stokes 5 2 1 1.0e-2
+./iterative-condensed-stokes 5 2 1 1.0e-3
+./iterative-condensed-stokes 5 2 1 1.0e-4
+./iterative-condensed-stokes 5 2 1 1.0e-5
+./iterative-condensed-stokes 5 2 1 1.0e-6
+
+./iterative-condensed-stokes 10 2 1 1.0e+1
+./iterative-condensed-stokes 10 2 1 1.0e-0
+./iterative-condensed-stokes 10 2 1 1.0e-1
+./iterative-condensed-stokes 10 2 1 1.0e-2
+./iterative-condensed-stokes 10 2 1 1.0e-3
+./iterative-condensed-stokes 10 2 1 1.0e-4
+./iterative-condensed-stokes 10 2 1 1.0e-5
+./iterative-condensed-stokes 10 2 1 1.0e-6
+
+./iterative-condensed-stokes 15 2 1 1.0e+1
+./iterative-condensed-stokes 15 2 1 1.0e-0
+./iterative-condensed-stokes 15 2 1 1.0e-1
+./iterative-condensed-stokes 15 2 1 1.0e-2
+./iterative-condensed-stokes 15 2 1 1.0e-3
+./iterative-condensed-stokes 15 2 1 1.0e-4
+./iterative-condensed-stokes 15 2 1 1.0e-5
+./iterative-condensed-stokes 15 2 1 1.0e-6
+
+./iterative-condensed-stokes 20 2 1 1.0e+1
+./iterative-condensed-stokes 20 2 1 1.0e-0
+./iterative-condensed-stokes 20 2 1 1.0e-1
+./iterative-condensed-stokes 20 2 1 1.0e-2
+./iterative-condensed-stokes 20 2 1 1.0e-3
+./iterative-condensed-stokes 20 2 1 1.0e-4
+./iterative-condensed-stokes 20 2 1 1.0e-5
+./iterative-condensed-stokes 20 2 1 1.0e-6
+
+./iterative-condensed-stokes 25 2 1 1.0e+1
+./iterative-condensed-stokes 25 2 1 1.0e-0
+./iterative-condensed-stokes 25 2 1 1.0e-1
+./iterative-condensed-stokes 25 2 1 1.0e-2
+./iterative-condensed-stokes 25 2 1 1.0e-3
+./iterative-condensed-stokes 25 2 1 1.0e-4
+./iterative-condensed-stokes 25 2 1 1.0e-5
+./iterative-condensed-stokes 25 2 1 1.0e-6
+
+./iterative-condensed-stokes 30 2 1 1.0e+1
+./iterative-condensed-stokes 30 2 1 1.0e-0
+./iterative-condensed-stokes 30 2 1 1.0e-1
+./iterative-condensed-stokes 30 2 1 1.0e-2
+./iterative-condensed-stokes 30 2 1 1.0e-3
+./iterative-condensed-stokes 30 2 1 1.0e-4
+./iterative-condensed-stokes 30 2 1 1.0e-5
+./iterative-condensed-stokes 30 2 1 1.0e-6
+
+./iterative-condensed-stokes 35 2 1 1.0e+1
+./iterative-condensed-stokes 35 2 1 1.0e-0
+./iterative-condensed-stokes 35 2 1 1.0e-1
+./iterative-condensed-stokes 35 2 1 1.0e-2
+./iterative-condensed-stokes 35 2 1 1.0e-3
+./iterative-condensed-stokes 35 2 1 1.0e-4
+./iterative-condensed-stokes 35 2 1 1.0e-5
+./iterative-condensed-stokes 35 2 1 1.0e-6
+
+./iterative-condensed-stokes 40 2 1 1.0e+1
+./iterative-condensed-stokes 40 2 1 1.0e-0
+./iterative-condensed-stokes 40 2 1 1.0e-1
+./iterative-condensed-stokes 40 2 1 1.0e-2
+./iterative-condensed-stokes 40 2 1 1.0e-3
+./iterative-condensed-stokes 40 2 1 1.0e-4
+./iterative-condensed-stokes 40 2 1 1.0e-5
+./iterative-condensed-stokes 40 2 1 1.0e-6
